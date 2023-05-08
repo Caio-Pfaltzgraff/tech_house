a611f85ce27590791f392f0884cae8b070e31151
Caio-Pfaltzgraff <caio.rllcr@gmail.com>
2023-05-08 17:19:15 -0300

    first commit

---
+++ src/main/java/br/com/caioprojects/TechHouse/TechHouseApplication.java
@@ -0,0 +1,13 @@
+package br.com.caioprojects.TechHouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechHouseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechHouseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/caioprojects/TechHouse/TechHouseApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.caioprojects.TechHouse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechHouseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
