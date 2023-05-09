d65483aef04a4f3af42cf9b81f5698ccdbed8e11
Caio-Pfaltzgraff <caio.rllcr@gmail.com>
2023-05-09 17:10:13 -0300

    listando os produtos do BD no html

--- src/main/java/br/com/caioprojects/TechHouse/controller/HomeController.java
+++ src/main/java/br/com/caioprojects/TechHouse/controller/HomeController.java
@@ -1,14 +1,24 @@
 package br.com.caioprojects.TechHouse.controller;
 
+import br.com.caioprojects.TechHouse.dto.DadosListagemProdutos;
+import br.com.caioprojects.TechHouse.repository.ProdutoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.List;
+
 @Controller
 public class HomeController {
 
+    @Autowired
+    private ProdutoRepository produtoRepository;
+
     @GetMapping("/home")
     public String home(Model model) {
+        List<DadosListagemProdutos> produtos = produtoRepository.findAll().stream().map(DadosListagemProdutos::new).toList();
+        model.addAttribute("produtos", produtos);
         return "home";
     }
 
