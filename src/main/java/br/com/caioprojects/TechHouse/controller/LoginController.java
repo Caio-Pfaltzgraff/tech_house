d880e3c16c4e16a410ee962f6a7072a8d4c2762d
Caio-Pfaltzgraff <caio.rllcr@gmail.com>
2023-07-03 19:48:17 -0300

    implementando login e logout

--- src/main/java/br/com/caioprojects/TechHouse/controller/LoginController.java
+++ src/main/java/br/com/caioprojects/TechHouse/controller/LoginController.java
@@ -1,16 +1,35 @@
 package br.com.caioprojects.TechHouse.controller;
 
+import br.com.caioprojects.TechHouse.domain.usuarios.Usuario;
+import br.com.caioprojects.TechHouse.domain.usuarios.UsuarioRepository;
+import br.com.caioprojects.TechHouse.service.CookieService;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class LoginController {
 
-    @GetMapping
-    @RequestMapping("/login")
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    @GetMapping("/login")
     public String login() {
         return "login";
     }
+
+//    @PostMapping("/logar")
+//    public String logar(Model model, String username, String password, HttpServletResponse response) {
+//        Usuario user = usuarioRepository.login(username, password);
+//        if(user != null) {
+//            CookieService.setCookie(response, "usuariosId", String.valueOf(user.getId()), 86400);
+//            return "redirect:/produto/inventory";
+//        }
+//        model.addAttribute("erro", "Usuário ou senha inválidos");
+//        return "login";
+//    }
 
 }
---
+++ src/main/java/br/com/caioprojects/TechHouse/security/SecurityConfig.java
@@ -0,0 +1,51 @@
+package br.com.caioprojects.TechHouse.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http.authorizeHttpRequests()
+                .requestMatchers("/css/**").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin(form -> form
+                        .loginPage("/login").defaultSuccessUrl("/produto/inventory", true)
+                        .permitAll()
+                )
+                .logout(logout -> logout
+                        .logoutUrl("/logout").logoutSuccessUrl("/login").invalidateHttpSession(true)
+                        .deleteCookies("JSESSIONID")
+                )
+                .csrf().disable().build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return new AutenticacaoService();
+    }
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public DaoAuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(userDetailsService());
+        authProvider.setPasswordEncoder(passwordEncoder());
+        return authProvider;
+    }
+}
